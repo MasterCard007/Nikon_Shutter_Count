0afb84b76bb2162c9254c8a27d1b5b8fd0877e57
MasterCard007 <enochliliwy@gmail.com>
2024-01-17 11:02:22 +0800

    ver 0.2

---
+++ setup.py
@@ -0,0 +1,28 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='NEFShutterCountReader',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple script to read the shutter count from Nikon NEF files',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='[Your Name]',
+    author_email='[Your Email]',
+    url='[Your Project URL]',
+    install_requires=[
+        'exifread',
+    ],
+    entry_points={
+        'console_scripts': [
+            'nef-shutter-count = nef_shutter_count_reader:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
